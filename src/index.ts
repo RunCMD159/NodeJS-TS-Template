87c2a4f26966b035043f0f838794c2add98d64d1
fschmuck <florian.schmuck@infonova.com>
2018-10-22 07:57:01 +0200

    added express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('test');
+import express = require('express');
+import { GreetingsController } from './app/controller/GreetingsController';
+
+
+const expressApp: express.Application = express();
+const port: string | number = process.env.PORT || 3000;
+
+expressApp.use('/greetings', GreetingsController);
+
+expressApp.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/`);
+});
+
